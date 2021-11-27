fdc4c28d717bbf508a96d73d1c2862e98e966a06
Moustafa Shomer <shomerthesec@gmail.com>
2021-11-27 16:53:36 +0200

    the full fetcher class

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,7 @@
 import numpy as np , pandas as pd 
 from sklearn.base import BaseEstimator, TransformerMixin
 
-class fetcher(BaseEstimator, TransformerMixin):
+class Fetcher(BaseEstimator, TransformerMixin):
     
     def __init__(self, building_id=122 , meter=0 , primary_use= 99 ): # 99 referes to none
         
@@ -14,9 +14,9 @@ class fetcher(BaseEstimator, TransformerMixin):
         return self  # nothing else to do
     
     
-    def transform(self, df):
-        
-        df= df.drop(columns=['Unnamed: 0' , 'precip_depth_1_hr' , 'cloud_coverage', 'site_id' , 'square_feet'])
+    def transform(self, x):
+        df= x.copy()
+        df= df.drop(['Unnamed: 0' , 'precip_depth_1_hr' , 'cloud_coverage', 'site_id' , 'square_feet'], axis=1)
         
         if self.primary_use == 99:
             
@@ -32,3 +32,16 @@ class fetcher(BaseEstimator, TransformerMixin):
         df.loc[:, "timestamp"] = pd.to_datetime( df.loc[:, "timestamp"] )
         df.set_index('timestamp', inplace=True)
         return df
+    
+    
+class Do_nothing(BaseEstimator, TransformerMixin):
+    
+    def __init__(self): # 99 referes to none
+        return 
+
+    def fit(self, x, y=None):
+        return self  # nothing else to do
+
+
+    def transform(self, x):
+        return x.astype(int)
